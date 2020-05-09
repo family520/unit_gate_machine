ff6d13c55e0e5758ee9b254c5cbedcf205a4c47c
smile <84656288@gamil.com>
2020-05-09 11:35:45 +0800

    基本页面

---
+++ js/setRem.js
@@ -0,0 +1,18 @@
+document.documentElement.style.fontSize =
+        document.documentElement.clientWidth / 7.5 + 'px';
+   
+(function(doc, win) {
+  var docEl = doc.documentElement,
+     resizeEvt =
+       'orientationchange' in window ? 'orientationchange' : 'resize'
+
+   function recalc() {
+     var clientWidth = docEl.clientWidth
+     if (!clientWidth) return
+     // clientWidth = clientWidth > 750 ? 750 : clientWidth
+     docEl.style.fontSize = clientWidth / 7.5 + 'px'
+   }
+   if (!doc.addEventListener) return
+   win.addEventListener(resizeEvt, recalc, false)
+   doc.addEventListener('DOMContentLoaded', recalc, false)
+ })(document, window)
